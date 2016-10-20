a4cc4155f19c6b535b1d2ed8124de7a64c952698
dhanajayan <dhananjayan322@gmail.com>
2016-10-20 23:54:55 +0530

    app with route

---
+++ app/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('Portfolio',["ngRoute"]);
+
+app.config(['$routeProvider',
+		function($routeProvider){
+		$routeProvider.when('/',{
+			// controller: 'homeController',
+			templateUrl: 'app/components/home/homeView.html',
+			controller: 'RouteController'
+		  }).
+		when('/route',{
+			templateUrl: 'app/components/home/homeView.html',
+			
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+}]);
+
+
+app.controller("RouteController", function($scope) {
+
+ });
