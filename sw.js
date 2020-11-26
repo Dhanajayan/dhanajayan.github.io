dbed5f656b013255c5324c487d7f9a267a7faefc
Dhananjayan <dhananjayan322@gmail.com>
2020-11-26 22:00:09 +0530

    optimized based on lighthouse report

--- sw.js
+++ sw.js
@@ -1,19 +1,15 @@
-  
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('video-store').then(function(cache) {
-     return cache.addAll([
-       './index.html',
-       './index.js',
-     ]);
-   })
- );
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("resume-cache").then(function (cache) {
+      return cache.addAll(["./index.html", "./index.js"]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener("fetch", function (e) {
   console.log(e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
